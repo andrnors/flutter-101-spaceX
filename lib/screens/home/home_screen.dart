8e0feeb057b7e57080ea24c87152831323da5b26
Andreas Norstein <andreasnorstein@gmail.com>
2021-03-08 19:35:34 +0100

    Start step 1

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,17 +1,17 @@
-
 import 'package:flutter/material.dart';
 
-import 'rockets/rocket_list.dart';
-
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    // Todo: Add  ann appBar to the Scaffold Widget with the title 'Rocket Guide'
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ROCKET GUIDE'),
+
+        // Todo: Replace the Center and Text Widget with the RocketList
+        body: Center(
+      child: Text(
+        'Hello Online! 🚀',
+        style: TextStyle(fontSize: 30),
       ),
-      body: RocketList()
-    );
+    ));
   }
 }
--- lib/screens/home/rockets/rocket_list.dart
+++ lib/screens/home/rockets/rocket_list.dart
@@ -1,35 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:rockets/screens/home/rockets/rocket_details_screen.dart';
-import 'package:rockets/services/rocket_service.dart';
-
-import 'rocket_tile.dart';
 
 class RocketList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: context.read<RocketService>().getRockets(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text('Houston we have a problem 👽');
-        } else if (!snapshot.hasData) {
-          return Center(child: CircularProgressIndicator());
-        } else {
-          final rockets = snapshot.data;
-          return ListView(children: [
-            for (final rocket in rockets)
-              RocketTile(
-                  rocket: rocket,
-                  onTap: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return RocketDetailsScreen(rocket: rocket);
-                    }));
-                  })
-          ]);
-        }
-      },
+    return Center(
+      child: Text(
+        'Rocket List View',
+        style: TextStyle(fontSize: 20),
+      ),
     );
   }
 }
