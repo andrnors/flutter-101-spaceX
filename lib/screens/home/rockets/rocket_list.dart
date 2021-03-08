270bab0d3a63b0340bace583dac9c2a59203c125
Andreas Norstein <andreasnorstein@gmail.com>
2021-03-08 20:14:05 +0100

    Start step 2

--- lib/screens/home/rockets/rocket_list.dart
+++ lib/screens/home/rockets/rocket_list.dart
@@ -1,32 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:rockets/screens/home/rockets/rocket_details_screen.dart';
 import 'package:rockets/services/rocket_service.dart';
-
-import 'rocket_tile.dart';
 
 class RocketList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
+      // Fetches data from RocketService
       future: context.read<RocketService>().getRockets(),
       builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text('Houston we have a problem 👽');
-        } else if (!snapshot.hasData) {
-          return Center(child: CircularProgressIndicator());
+        if (snapshot.hasError) { // Check if we encounter any errors while fetching data
+          return Center(); // Todo: Add a Text for an error message if fetching data fails
+        } else if (!snapshot.hasData) { // While we are loading data
+          return Center(); // Todo: Add a loading Widget. TIP: CircularProgressIndicator is really nice for this
         } else {
+          // Data is finally ready in the snapshot
           final rockets = snapshot.data;
+          // TODO: Iterate through all Rockets and create a RocketTile with the rocket
+          // HINT: Leave the onTap-function for the RocketTile as null for now.
           return ListView(children: [
-            for (final rocket in rockets)
-              RocketTile(
-                  rocket: rocket,
-                  onTap: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return RocketDetailsScreen(rocket: rocket);
-                    }));
-                  })
+
           ]);
         }
       },
